eaa45328d46edadc8c963180fd8658f37d2cab44
Stas <rsl14591@gmail.com>
2022-04-11 14:51:56 +0300

    1st lesson

---
+++ script/script.js
@@ -0,0 +1,82 @@
+// console.log("Hello world!");
+// let x = 7;
+// console.log(x);
+// x = "bye";
+// console.log(x);
+// const pi = 3.141125125;
+// console.log(pi);
+// // pi=3.14; - impossible
+// x = true;
+// x = null;
+// console.log(x);
+// let res = 7 % 3;
+// console.log(res);
+// x = '2' == 2;
+// console.log(x);
+// x = '2' === 2;
+// console.log(x);
+
+// if (3 != '3') {
+//     console.log('as true');
+// }
+// else{
+//     console.log('as false');
+// }
+
+// let age = 18;
+
+// let v = age >= 20 ? 42 : 1.5;
+// console.log(v);
+
+// res = 5;
+// switch (res) {
+//     case 5:
+//         console.log(`number ${res}`);
+//         break;
+//         case '5':
+//         console.log(`string ${res}`);
+//         break;
+//     default:
+//         console.log(`not a five`);
+//         break;
+// }
+
+// for(let i = 1; i <= 5; i ++){
+//     console.log(i);
+// }
+
+// console.log(typeof res);
+
+// res = "5";
+// console.log(typeof res);
+
+// res = true;
+// console.log(typeof res);
+
+// res = undefined;
+// console.log(typeof res);
+
+// res = null;
+// console.log(typeof res);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////FUNCTIONS///////////////////////////////////////////////////////////////
+
+function add(a, b){
+    return a + b;
+} 
+
+
+//  HW:
+/*
+let res = sumDigits(1234);
+let res = luckyNumber(123870);
+
+fuction sumDigits(x){
+    returen !!res = 10!!;
+}
+
+function luckyNumber(x){
+    return  !!res ? "lucky" : "unlucky"!!
+}
+*/
