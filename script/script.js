57312619cd7758608b6f0c027b248b3872934311
Stas <rsl14591@gmail.com>
2022-04-24 17:57:04 +0300

    12L

--- script/script.js
+++ script/script.js
@@ -1,38 +1,4 @@
-//  HW:
-/*
-let res = sumDigits(1234);
-let res = luckyNumber(123870);
-
-fuction sumDigits(x){
-    returen !!res = 10!!;
-}
-
-function luckyNumber(x){
-    return  !!res ? "lucky" : "unlucky"!!
-}
-*/
-
-
-function sumDigits(n){
-    let sum = 0;
-    for ( ; n != 0; n = Math.floor(n / 10)) {
-    sum += n % 10;
-    }
-    return sum;
-}
-
-let res = sumDigits(1234);
-console.log(`first task : ${res}`);
-
-function luckyNumber(x){
-    let rem = 0, even = 0, odd = 0;
-    for( ; x != 0; x = Math.floor(x / 10)){
-        rem = x % 10;
-        rem % 2 == 0 ? even += rem : odd += rem;
-    }
-    even === odd ? rem = "lucky" : rem = "unlucky";
-    return rem;
-}
-
-res = luckyNumber(123870);
-console.log(`second task : ${res}`);
+let x = 5; 
+console.log(typeof x);
+x = String(x);
+console.log(typeof x);
