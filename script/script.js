bc5b21f10d6cb96afd86fde243af44e1c364bffb
Stas <rsl14591@gmail.com>
2022-05-01 20:32:49 +0300

    methods

--- script/script.js
+++ script/script.js
@@ -1,65 +1,100 @@
-const arr = [9, 2, 4, 1, 5, 2, 9, 1, 2, 0];
-console.log('before');
-printArray(arr);
-bubbleSort(arr);
-console.log('after');
-printArray(arr);//0,1,1,2,2,2,4,5,9,9
-const persons = [new Person(3000, 'Mary', 'Smith', 18), new Person(4000, 'Tigran', 'Petrosian', 42),
-new Person(2000, 'Peter', 'Jackson', '42'), new Person(1000, 'John', 'Smith', 27)];
-console.log('before');
-printArray(persons);
-bubbleSortByAge(persons);
-console.log('after');
-printArray(persons);
+//number
+let x = 10.4;
+let res = Math.round(x);
+console.log(res);
+res = Math.trunc(x);
+console.log(res);
+res = Math.floor(x);
+console.log(res);
+console.log(Math.PI);
+res = Math.max(7, 2, 9, 11, 4);
+console.log(res);
+res = Math.pow(3, 4);
+console.log(res);
+res = Math.random();
+console.log(res);
+console.log(rndm(1, 4));
+console.log('=========================');
+//////////////////////////////////////////////////////////////////////////
+//String
+x = '             Two beer or not two beer                    ';
+console.log(x);
+x = x.trim();
+console.log(x);
+res = x.split(' ');
+console.log(res);
+res = x.toUpperCase();
+console.log(res);
+res = x.toLowerCase();
+console.log(res);
+res = x.length;
+console.log(res);
+res = x.charAt(1);
+console.log(res);
+res = x[1];
+console.log(res);
+res = x.indexOf('e');
+console.log(res);
+res = x.lastIndexOf('e');
+console.log(res);
+res = x.startsWith('Two');
+console.log(res);
+res = x.substring(4, 8);
+console.log(res);
+res = x.slice(4, 8);
+console.log(res);
+console.log('=========================');
+//////////////////////////////////////////////////////////////////////////
+//arrays
+const arr = [1, 2, 3, 'four', 'five'];
+console.log(typeof arr);
+arr.push(6);
+console.log('===push===');
+console.log(arr);
+console.log('===pop===');
+res = arr.pop();
+console.log(arr);
+console.log(res);
+console.log('===shift===');
+res = arr.shift();
+console.log(arr);
+console.log(res);
+console.log('===unshift===');
+arr.unshift(1);
+console.log(arr);
+const arr1 = ['one', 'two', 'three'];
+const arr2 = ['four', 'five'];
+console.log('===concat===');
+const arr3 = arr1.concat(arr2);
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+console.log('===slice===');
+res = arr.slice(2, 4);
+console.log(arr);
+console.log(res);
+console.log('===splice===');
+//arr.splice(1,2, -10, -20, -30);
+//arr.splice(1, 0, -30);
+//arr.splice(0, 1);//arr.shift()
+//arr.splice(0, 0, -10, -20);//arr.unshift()
+//arr.splice(arr.length - 1, 1);//arr.pop()
+arr.splice(arr.length, 0, 100);//arr.push()
+console.log(arr);
+//////////////////////////////////////////////////////////////////////////
+//modals
+// alert('Hello');
 
-function bubbleSort(arr) {
-    for (let j = 0; j < arr.length-1; j++) {
-        for (let i = 0, rem; i < arr.length-1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                rem = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = rem;
-            }
-        }
-    }
+// const check = confirm('are you elder then 18?');
+// alert(check);
+
+// const nickname = prompt('What is your name');
+// alert(nickname);
+//////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////
+function rndm(a, b) {
+    return Math.trunc(Math.random() * (b - a) + a);
 }
 
-function bubbleSortByAge(persons) {
-    for (let j = 0; j < persons.length-1; j++) {
-        for (let i = 0, rem; i < persons.length - 1; i++) {
-            if (+persons[i].age > +persons[i + 1].age) {
-                rem = persons[i];
-                persons[i] = persons[i + 1];
-                persons[i + 1] = rem;
-            }
-        }
-    }
-}
-
-function printArray(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
-    }
-    console.log('==========================');
-}
-
-function printObject(obj) {
-    for (let k in obj) {
-        if (typeof obj[k] === 'function') {
-            console.log(`${k} -> ${obj[k]()}`);
-        } else {
-            console.log(`${k} -> ${obj[k]}`);
-        }
-    }
-    console.log('======================');
-}
-
-function Person(id, firstName, lastName, age) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = +age;
-    this.fullName = function () {
-        return `${this.firstName} ${this.lastName}`;
-    }
-}
