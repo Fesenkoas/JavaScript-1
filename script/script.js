ba518636affb62ea4d2489a8a7dc2fd500e9ac6f
Stas <rsl14591@gmail.com>
2022-05-11 13:15:19 +0300

    repaired

--- script/script.js
+++ script/script.js
@@ -1,58 +1,3 @@
-// ////////////////////////////////////
-// ////////////////BOM/////////////////
-// ////////////////////////////////////
-// //console.log(window); object BOM
-// //window.open('https://www.tel-ran.com/'); - open side-cite
-// // console.log(navigator.userAgent);
-// // console.log(navigator.userAgentData.platform);
-// // console.log(location.origin);
-// ////////////////////////////////////
-// ////////////////DOM/////////////////
-// ////////////////////////////////////
-// // console.log(document.body.innerHTML);
-// const div1 = document.getElementById('div1');
-// //console.log(div1.innerHTML);
-// div1.style.border = '1px solid black';
-
-// const pdiv1 = document.querySelectorAll('#div1 > p');
-// for (let i = 0; i < pdiv1.length; i++) {
-//     pdiv1[i].innerHTML = `<span> new text ${i + 1} </span>`;
-// }
-
-// const children = div1.children;
-// console.log(children.length);
-// console.log(children[0] === pdiv1[0]);
-
-// for (let i = 0; i < children.length; i++) {
-//     children[i].style.color = 'red';
-// }
-
-// div1.firstElementChild.style.color = 'green';
-
-// const newp1 = document.createElement('p');
-// const newp1text = document.createTextNode('This text created with special method');
-// newp1.appendChild(newp1text);
-// // div1.appendChild(newp1);
-// document.body.insertBefore(newp1, div1.nextElementSibling);
-
-// const newp2 = document.createElement('p');
-// const newp2text = document.createTextNode('p2 text');
-// newp2.appendChild(newp2text);
-
-// div1.replaceChild(newp2, div1.firstChild);
-
-// div1.removeChild(div1.firstElementChild.nextElementSibling);
-
-// //date
-// const d = new Date();
-// console.log(d);
-// console.log(d.getHours(), d.getMinutes(), d.getSeconds());
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-/////////////////////////////////
-//library////////////////////////
-/////////////////////////////////
 const library = [];
 let inputData = prompt('Enter book data separate by ";" with four fildes like "isbn;title;author;year"');
 
@@ -62,12 +7,11 @@ while (inputData) {
         let check = findBook(library, arg[0]);
         check === -1 ? library.push(new Book(arg[0], arg[1], arg[2], arg[3])) : alert(`The position of ISBN of your book in library is ${check}`);
         inputData = prompt('Enter book data separate by ";" with four fildes like "isbn;title;author;year"');
-
     }
     else {
         alert('ERROR: DATA is filled incorrectly');
         inputData = prompt('Enter book data separate by ";" with four fildes like "isbn;title;author;year"');
-    }
+    } 
 }
 
 printLibrary(library);
@@ -84,14 +28,9 @@ function findBook(library, isbn) {
 function printLibrary(library) {
     for (let i = 0; i < library.length; i++) {
         let p = document.createElement('p');
-        let time = document.createTextNode(`Book number ${i+1} : ${library[i].toString()}`);
-        p.appendChild(time);
-        if (newbook.firstElementChild) {
-            newbook.appendChild(p, newbook.firstElementChild);
-        }
-        else {
-            newbook.appendChild(p);
-        }
+        let book = document.createTextNode(`Book number ${i+1} : ${library[i].toString()}`);
+        p.appendChild(book);
+        newbook.appendChild(p, newbook.firstElementChild);
     }
 
 }
