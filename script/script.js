1f9023ed46401facf3f7ba0bbcf8c18fee12b792
Stas <rsl14591@gmail.com>
2022-05-11 20:05:55 +0300

    dom2 -1

--- script/script.js
+++ script/script.js
@@ -1,45 +1,13 @@
-const library = [];
-let inputData = prompt('Enter book data separate by ";" with four fildes like "isbn;title;author;year"');
+const box = document.querySelector('#box');
+const container = document.querySelector('#container');
+const t = setInterval(move, 5);
+let pos = 0;
 
-while (inputData) {
-    let arg = inputData.split(';');
-    if (arg.length === 4) {
-        let check = findBook(library, arg[0]);
-        check === -1 ? library.push(new Book(arg[0], arg[1], arg[2], arg[3])) : alert(`The position of ISBN of your book in library is ${check}`);
-        inputData = prompt('Enter book data separate by ";" with four fildes like "isbn;title;author;year"');
-    }
-    else {
-        alert('ERROR: DATA is filled incorrectly');
-        inputData = prompt('Enter book data separate by ";" with four fildes like "isbn;title;author;year"');
-    } 
-}
-
-printLibrary(library);
-
-function findBook(library, isbn) {
-    for (let i = 0; i < library.length; i++) {
-        if (library[i].isbn === isbn) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-function printLibrary(library) {
-    for (let i = 0; i < library.length; i++) {
-        let p = document.createElement('p');
-        let book = document.createTextNode(`Book number ${i+1} : ${library[i].toString()}`);
-        p.appendChild(book);
-        newbook.appendChild(p);
-    }
-}
-
-function Book(isbn, title, author, year) {
-    this.isbn = isbn;
-    this.title = title;
-    this.author = author;
-    this.year = +year;
-    this.toString = function () {
-        return `ISBN : ${this.isbn}, Title : ${this.title}, Author : ${this.author}, Year of publishing : ${this.year}`;
+function move(){
+    pos++;
+    box.style.left = pos+'px';
+    box.style.top = pos+'px';
+    if(pos >= container.clientHeight - box.clientHeight){
+        clearInterval(t);
     }
 }
