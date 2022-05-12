66b82ecc1468f22230607d2cb4554e2419b2c9fc
Stas <rsl14591@gmail.com>
2022-05-12 07:57:30 +0300

    my hw

--- script/script.js
+++ script/script.js
@@ -1,13 +1,30 @@
 const box = document.querySelector('#box');
 const container = document.querySelector('#container');
 const t = setInterval(move, 5);
-let pos = 0;
 
-function move(){
-    pos++;
-    box.style.left = pos+'px';
-    box.style.top = pos+'px';
-    if(pos >= container.clientHeight - box.clientHeight){
-        clearInterval(t);
+let pos = 0;
+let switcher = true;
+
+function move() {
+    if (switcher) {
+        pos++;
+        box.style.left = pos + 'px';
+        box.style.top = pos + 'px';
+        if (pos >= container.clientHeight - box.clientHeight) {
+            clearInterval(t);
+            switcher = false;
+        }
     }
+    else{
+        pos--;
+        box.style.left = pos + 'px';
+        box.style.top = pos + 'px';
+        if (pos === 0) {
+            switcher = true;
+        }
+    }
+}
+
+function boom(){
+    clearInterval(t);
 }
