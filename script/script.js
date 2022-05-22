82bbb00fd6bab05dd5d08103b02f70210c7230ad
Stas <rsl14591@gmail.com>
2022-05-22 20:58:37 +0300

    IMPORTANT try/catch

--- script/script.js
+++ script/script.js
@@ -1,8 +1,36 @@
-let i = -1;
-const fn = () => {
-    let i = 1;
-    console.log(i);
+let person = {
+    name: 'Peter',
+}
+person = null;
+printName(person);
+console.log('===========================');
+
+try {
+    let res = solution(0, 10);
+    console.log(res);
+} catch (e) {
+    console.log(e.message);
 }
 
-fn();
-console.log(i);
+function solution(a, b) {
+    if (a !== 0) {
+        return b / a;
+    }
+    if (b === 0) {
+        throw new Error('solution any number');
+    }
+    throw {message : 'no solution'};
+}
+
+function printName(obj) {
+    try {
+        console.log(obj.name);
+    } catch (e) {
+        //console.log(e.message);
+        console.log('error');
+    } finally {
+        console.log('Finally in printName');
+    }
+}
+
+
